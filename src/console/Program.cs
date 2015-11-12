09f13d10a27defb1f90ae8f3af3c300f1f90879e
John Luo <jt.luo@mail.utoronto.ca>
2015-11-12 15:38:03 -0800

    moving things around

---
+++ src/console/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("SQLite version number: " + SQLiteMethods.sqlite3_libversion_number());
+
+        }
+    }
+
+    public static class SQLiteMethods
+    {
+        [DllImport("libuv")]
+        public static extern int sqlite3_libversion_number();
+    }
+}
