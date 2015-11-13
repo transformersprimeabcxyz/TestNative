1080be644aa6d54e70ef391a14999b255aea243f
John Luo <jt.luo@mail.utoronto.ca>
2015-11-12 16:08:43 -0800

    Testing loading libuv

--- src/console/Program.cs
+++ src/console/Program.cs
@@ -9,14 +9,20 @@ namespace console
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello world");
-            Console.WriteLine("SQLite version number: " + SQLiteMethods.sqlite3_libversion_number());
-
+            Console.WriteLine("SQLite version: " + Marshal.PtrToStringAnsi(SQLiteMethods.sqlite3_libversion()));
+            Console.WriteLine("libuv version: " + Marshal.PtrToStringAnsi(LibuvMethods.uv_version_string()));
         }
     }
 
     public static class SQLiteMethods
     {
         [DllImport("sqlite3")]
-        public static extern int sqlite3_libversion_number();
+        public static extern IntPtr sqlite3_libversion();
+    }
+
+    public static class LibuvMethods
+    {
+        [DllImport("libuv")]
+        public static extern IntPtr uv_version_string();
     }
 }
